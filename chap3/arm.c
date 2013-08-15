704edf166bebb93a6b5ffb1436c15fa00e7b86e8
cwnjitwx071 <cwnjitwx071@163.com>
2013-08-15 23:10:27 +0800

    Signed-off-by: cwnjitwx071 <cwnjitwx071@163.com>

--- chap3/arm.c
+++ chap3/arm.c
@@ -3,4 +3,3 @@ read 4 bytes: "ccff" from file:./test.txt
 read 6 bytes: "ccffcc" from file:./test.txt
 generated a child process:6536;fd_father:3
 read 5 bytes:"ccffc" from file:./test.txt
-read 7 bytes: "ccffcff" from file:./test.txt
